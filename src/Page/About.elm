2ae1cebc2cdf8e0a7804ef255cffe36abef76835
ahdyt <34616952+ahdyt@users.noreply.github.com>
2021-10-08 20:38:10 +0700

    add disclaimer

--- src/Page/About.elm
+++ src/Page/About.elm
@@ -14,6 +14,10 @@ view =
         [ div [ class "container" ]
             [ h1 [] [ text "Tentang" ]
             , p [] [ text "Perkumpulan mahasiswa pegiat ", a [ href "/todos" ] [ text "pemrograman." ] ]
+            , p [] [ text "Kami independen, tidak terikat dengan nama kampus atau organisasi intra/ekstra kampus manapun." ]
+            , p [] [ text "Karena founder member dari kampus UIN Walisongo Semarang, kami sementara membuka bagi mahasiswa UIN Walisongo Semarang terutama Teknologi Informasi." ]
+            , p [] [ text "Kami terbuka untuk sharing ilmu seputar teknologi informasi, pemrograman, maupun teknologi-teknologi terbaru lainnya." ]
+            , p [] [ text "Member tidak mencerminkan member sebagai mahasiswa kampus X, jadi tidak ada hubungannya dengan politik, ekonomi, sosial, budaya, akademis, dan lainnya dari kampus X." ]
             ]
         ]
     }
